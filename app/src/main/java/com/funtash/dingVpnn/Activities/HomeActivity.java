0a4ee37e0ee6c839a7e1046de4a109c3c8c9dc82
Asghar Ali <60741664+Asghar-ali-code@users.noreply.github.com>
2021-11-22 13:04:29 +0500

    with font

--- app/src/main/java/com/funtash/dingVpnn/Activities/HomeActivity.java
+++ app/src/main/java/com/funtash/dingVpnn/Activities/HomeActivity.java
@@ -2,7 +2,11 @@ package com.funtash.dingVpnn.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.text.TextPaint;
 import android.view.LayoutInflater;
 
 import com.funtash.dingVpnn.R;
@@ -16,5 +20,14 @@ ActivityHomeBinding binding;
         super.onCreate(savedInstanceState);
         binding= ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        TextPaint paint = binding.TvTop.getPaint();
+        float width = paint.measureText("Di, ng");
+
+        Shader textShader = new LinearGradient(0, 0, width, binding.TvTop.getTextSize(),
+                new int[]{
+                        Color.parseColor("#db4e2d"),
+                        Color.parseColor("#f73607"),
+                }, null, Shader.TileMode.CLAMP);
+       binding.TvTop.getPaint().setShader(textShader);
     }
 }
--- app/src/main/java/com/funtash/dingVpnn/Activities/MainActivity.java
+++ app/src/main/java/com/funtash/dingVpnn/Activities/MainActivity.java
@@ -2,15 +2,19 @@ package com.funtash.dingVpnn.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.funtash.dingVpnn.R;
+import com.funtash.dingVpnn.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
-
+ ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.button.setOnClickListener(view -> {startActivity(new Intent(this,HomeActivity.class));});
     }
 }
